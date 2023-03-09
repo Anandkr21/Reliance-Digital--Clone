a81547afce09a7ce50f6628b0a126c9bb402a1b2
Anandkr21 <anandkumargupta.akg15@gmail.com>
2023-03-09 11:07:48 +0530

    seperate js file for all created

---
+++ scripts/login.js
@@ -0,0 +1,12 @@
+let lsdata = localStorage.getItem('data') || []
+    document.querySelector('form').addEventListener('submit',myfun)
+
+    function myfun(event){
+        event.preventDefault()
+
+        let mbl=document.querySelector('#mobile').value;
+        localStorage.setItem('data', mbl)
+        window.location.href='./register.html'
+    }
+    
+    
