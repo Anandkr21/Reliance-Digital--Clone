207e669caa46d37d0c03831f283df75fd54f9b02
Anandkr21 <anandkumargupta.akg15@gmail.com>
2023-03-23 01:24:35 +0530

    gif path changes

--- scripts/register.js
+++ scripts/register.js
@@ -1,11 +1,11 @@
 let lsdata = localStorage.getItem('data') || []
 
-document.querySelector('#mob').value=lsdata;
+document.querySelector('#mob').value = lsdata;
 
 document.querySelector('form').addEventListener('submit', myfun1)
 
-function myfun1(event){
+function myfun1(event) {
    event.preventDefault();
    alert("You have Successfully Registerd ! ✅")
-   window.location='index.html'
+   window.location = 'index.html'
 }
